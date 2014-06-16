822ac97c87348c5e13b11bb07373e6d85c05b8a4
Zhang,Jingning <zhangjn.uiuc@gmail.com>
2014-06-16 16:25:29 -0500

    Reorganize nodes and links data objects with Javascript prototype

---
+++ public_html/js/XDI_graph_editor_objects.js
@@ -0,0 +1,73 @@
+/* 
+The MIT License (MIT)
+
+Copyright (c) 2014 Neustar Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+function XDIElement () {
+	this.id = -1;
+	this.isAdded = false;
+	this.name = "";
+	this.shortName = "";
+}
+
+function XDINode (id, name, shortName, type) {
+	this.id = id;
+	this.name = name;
+	this.shortName = shortName;
+	this.type = type;
+	this.isRoot = false;
+	this.parents = [];
+	this.children = [];
+}
+XDINode.prototype = new XDIElement();
+
+function XDILink (id,name,shortName,left, right, source, target)
+{
+	this.id = id;
+	this.name = name;
+	this.shortName = shortName;
+	this.left = left;
+	this.right = right;
+	this.source = source;
+	this.target = target;
+
+	this.isRel = false;
+}
+XDILink.prototype = new XDIElement();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
